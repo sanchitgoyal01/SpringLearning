db474154c3f065315e6adda09ea249779942df9b
Goyal, Sanchit <sanchit.goyal@ndus.edu>
2015-08-26 09:25:52 -0500

    Base Commit

---
+++ src/edu/ndus/cts/eswa/sandbox/DrawingApp.java
@@ -0,0 +1,18 @@
+package edu.ndus.cts.eswa.sandbox;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
+
+@SuppressWarnings("deprecation")
+public class DrawingApp {
+
+	public static void main(String[] args) {
+//		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		Triangle triangle = (Triangle) context.getBean("triangle-alias");
+		triangle.draw();
+	}
+}
---
+++ src/edu/ndus/cts/eswa/sandbox/Triangle.java
@@ -0,0 +1,21 @@
+package edu.ndus.cts.eswa.sandbox;
+
+import java.util.List;
+
+public class Triangle {
+	private List<Point> points;
+	
+	public List<Point> getPoints() {
+		return points;
+	}
+
+	public void setPoints(List<Point> points) {
+		this.points = points;
+	}
+
+	public void draw() {
+		for(Point point : points) {
+			System.out.println(point.toString());
+		}
+	}
+}
